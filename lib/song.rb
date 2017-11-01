798dd4717f24c232b1f29fcd0191074e5ca7c9a9
Joshua Needham <joshuabneedham@gmail.com>
2017-11-01 21:46:20 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+  @@count = 0
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def artist
+    @artist
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def self.count
+    @@count
+  end
+    
+end
